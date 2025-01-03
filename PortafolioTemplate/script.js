f9a72fbfac81e688aa9e15c75a5f1c5d661c17e5
JuanDiego3030 <juandiegoaranaperez@gmail.com>
2025-01-02 22:51:53 -0400

    Primer commit

---
+++ PortafolioTemplate/script.js
@@ -0,0 +1,6 @@
+AOS.init({
+    duration: 1500, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de transición
+    once: true, // Animar solo una vez
+    offset: 120 // Activar animación cuando el elemento esté a 120px del viewport
+  });
---
+++ app1/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+
+def index (request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = ('Consulta')
+        message = request.POST.get('message')
+        
+        print(f"Nombre: {name}, Email: {email}, Mensaje: {message}")  # Para depurar
+        
+        # Enviar correo
+        try:
+            send_mail(
+                subject,
+                f'Mensaje de {name} <{email}>:\n\n{message}',
+                'juanxcosa@gmail.com',  # Cambia esto por tu correo
+                ['martinhermoso14@gmail.com'],  # Cambia esto por el correo del destinatario
+                fail_silently=False,
+            )
+            messages.success(request, 'Mensaje enviado con éxito.')
+            return redirect('index')  # Redirige a la vista de índice
+        except Exception as e:
+            print(e)  # Imprimir el error para depurar
+            messages.error(request, 'Hubo un error al enviar tu mensaje. Inténtalo de nuevo.')
+
+    return render (request, "index.html")
+
